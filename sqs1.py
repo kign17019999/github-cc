27bc8bf1cd6a5756d191ea1b9a9ba90f6e9cac0d
kign17019999 <purintanirat@hotmail.com>
2022-12-28 21:47:30 +0000

    2

--- sqs1.py
+++ sqs1.py
@@ -1,1 +1,16 @@
-print('hello world')
+import boto3
+
+# Create an SQS client
+sqs = boto3.client('sqs')
+
+# Set the URL of the queue
+queue_url = 'https://sqs.us-east-1.amazonaws.com/123456789012/my-queue'
+
+# Send a message to the queue
+response = sqs.send_message(
+    QueueUrl=queue_url,
+    MessageBody='Hello, world!'
+)
+
+# Print the message ID
+print(response['MessageId'])
