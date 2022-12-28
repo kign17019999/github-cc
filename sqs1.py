14533176efd4d52a39c1aecc45d32459f1431221
kign17019999 <purintanirat@hotmail.com>
2022-12-28 21:55:47 +0000

    1

--- sqs1.py
+++ sqs1.py
@@ -1,16 +1,27 @@
 import boto3
 
 # Create an SQS client
-sqs = boto3.client('sqs')
-
-# Set the URL of the queue
-queue_url = "https://sqs.us-east-1.amazonaws.com/183243280383/queue1"
+sqs = boto3.client('sqs', region_name='us-east-1')
 
 # Send a message to the queue
 response = sqs.send_message(
-    QueueUrl=queue_url,
-    MessageBody='Hello, world!'
+    QueueUrl='"https://sqs.us-east-1.amazonaws.com/183243280383/queue1",
+    MessageBody='Hello, World!'
 )
 
 # Print the message ID
 print(response['MessageId'])
+
+# Receive a message from the queue
+response = sqs.receive_message(
+    QueueUrl='"https://sqs.us-east-1.amazonaws.com/183243280383/queue1",
+    MaxNumberOfMessages=1
+)
+
+# Print the message body
+print(response['Messages'][0]['Body'])
+
+
+I
+
+
