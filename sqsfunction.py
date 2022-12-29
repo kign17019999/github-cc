ac1dde6fb882346a1cb0c04a251b9d7dc9c32031
kign17019999 <purintanirat@hotmail.com>
2022-12-29 22:23:23 +0000

    2

--- sqsfunction.py
+++ sqsfunction.py
@@ -6,18 +6,24 @@ class SQSFunction:
         self.sqs = boto3.client('sqs', region_name=region_name)
         self.queue_url = queue_url
 
-    def send_message(self, message, message_attributes):
-        # Serialize the message using json
-        serialized_message = json.dumps(message)
-
-        # Send the message with message attributes
-        response = self.sqs.send_message(
+    def send_messages(self, messages):
+        # Create a list to store the message bodies
+        message_bodies = []
+        
+        # Iterate over the messages and add their bodies to the list
+        for message in messages:
+            # Serialize the message using json
+            serialized_message = json.dumps(message)
+            message_bodies.append(serialized_message)
+        
+        # Send the batch of messages all at once
+        response = self.sqs.send_message_batch(
             QueueUrl=self.queue_url,
-            MessageBody=serialized_message,
-            MessageAttributes=message_attributes
+            Entries=[{'Id': str(i), 'MessageBody': message} for i, message in enumerate(message_bodies)]
         )
-
-        return response['MessageId']
+        
+        # Return the list of message IDs
+        return [response['Successful'][i]['MessageId'] for i in range(len(response['Successful']))]
 
     def receive_message(self, message_attribute_names):
         # Receive a message from the queue
