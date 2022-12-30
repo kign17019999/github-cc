25d236bc49d2b07cbfe5e540472f172f78539d8f
kign17019999 <purintanirat@hotmail.com>
2022-12-30 23:46:46 +0000

    1

--- delete_message_in_queue.py
+++ delete_message_in_queue.py
@@ -1,8 +1,47 @@
 from sqsfunction import SQSFunction
 import numpy as np
 
+print('trying queue2 ...')
 # Set the URL of the queue
 queue_url = 'https://sqs.us-east-1.amazonaws.com/183243280383/queue2'
+region_name = 'us-east-1'
+
+sqs_function = SQSFunction(queue_url, region_name)
+
+# Receive all messages in the queue
+while True:
+    # Receive the message with the message attributes
+    message = sqs_function.receive_message()
+
+    if message is not None:
+        print(f'Message received: {message}')
+    else:
+        # Stop receiving messages if there are no more messages in the queue
+        break
+        
+#******************************************************************************************************#      
+print('trying queue_to_worker ...')        
+# Set the URL of the queue
+queue_url = 'https://sqs.us-east-1.amazonaws.com/183243280383/queue_to_worker'
+region_name = 'us-east-1'
+
+sqs_function = SQSFunction(queue_url, region_name)
+
+# Receive all messages in the queue
+while True:
+    # Receive the message with the message attributes
+    message = sqs_function.receive_message()
+
+    if message is not None:
+        print(f'Message received: {message}')
+    else:
+        # Stop receiving messages if there are no more messages in the queue
+        break
+
+#******************************************************************************************************#
+print('trying queue_to_master ...')         
+# Set the URL of the queue
+queue_url = 'https://sqs.us-east-1.amazonaws.com/183243280383/queue_to_master'
 region_name = 'us-east-1'
 
 sqs_function = SQSFunction(queue_url, region_name)
