7a68ff7a7d5d3ebd05783f4614556fa6cd82955a
kign17019999 <purintanirat@hotmail.com>
2022-12-31 15:38:10 +0000

    1

--- read_no_delete.py
+++ read_no_delete.py
@@ -1,18 +1,58 @@
 from sqsfunction import SQSFunction
 import numpy as np
 
+print('trying queue2 ...')
 # Set the URL of the queue
 queue_url = 'https://sqs.us-east-1.amazonaws.com/183243280383/queue2'
 region_name = 'us-east-1'
 
-# Create an instance of the SQSFunction class
 sqs_function = SQSFunction(queue_url, region_name)
 
-# Receive the message w/ the message attributes
-message, message_attributes = sqs_function.receive_message_no_delete(attribute_name='Worker', attribute_value='11')
+# Receive all messages in the queue
+while True:
+    # Receive the message with the message attributes
+    message = sqs_function.receive_message_no_delete()
 
-if message is not None:
-    print(f'Message received: {message}')
-    print(f'Message attributes: {message_attributes}')
-else:
-    print('No messages in the queue')
+    if message is not None:
+        print(f'Message received: {message}')
+    else:
+        # Stop receiving messages if there are no more messages in the queue
+        break
+        
+#******************************************************************************************************#      
+print('trying queue_to_worker ...')        
+# Set the URL of the queue
+queue_url = 'https://sqs.us-east-1.amazonaws.com/183243280383/queue_to_worker'
+region_name = 'us-east-1'
+
+sqs_function = SQSFunction(queue_url, region_name)
+
+# Receive all messages in the queue
+while True:
+    # Receive the message with the message attributes
+    message = sqs_function.receive_message_no_delete()
+
+    if message is not None:
+        print(f'Message received: {message}')
+    else:
+        # Stop receiving messages if there are no more messages in the queue
+        break
+
+#******************************************************************************************************#
+print('trying queue_to_master ...')         
+# Set the URL of the queue
+queue_url = 'https://sqs.us-east-1.amazonaws.com/183243280383/queue_to_master'
+region_name = 'us-east-1'
+
+sqs_function = SQSFunction(queue_url, region_name)
+
+# Receive all messages in the queue
+while True:
+    # Receive the message with the message attributes
+    message = sqs_function.receive_message_no_delete()
+
+    if message is not None:
+        print(f'Message received: {message}')
+    else:
+        # Stop receiving messages if there are no more messages in the queue
+        break
