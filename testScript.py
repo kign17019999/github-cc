c10f0310589ee26ee6a17cc13c70095685af1331
kign17019999 <purintanirat@hotmail.com>
2023-01-06 04:29:13 +0000

    1

--- testScript.py
+++ testScript.py
@@ -49,7 +49,7 @@ def main():
         }
     always_on = [inst_worker_id['1'], inst_worker_id['2']]
     start_by_off = [inst_worker_id['3'], inst_worker_id['4'], inst_worker_id['5'], inst_worker_id['6'], inst_worker_id['7'], inst_worker_id['8']]
-    id_cowork = [inst_worker_id['1']]
+    id_cowork = inst_worker_id['1']
     id_list = [inst_worker_id['2']]
     #id_list = ['i-055336f9cd0657c5c']
     
